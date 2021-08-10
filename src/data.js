e149c60d47f2b763a7ad6d7df010f2a25e56d8d4
YudexiOrozco <86724658+YudexiOrozco@users.noreply.github.com>
2021-08-09 23:45:30 -0500

    Primeros avances del proyecto

--- src/data.js
+++ src/data.js
@@ -1,9 +1,95 @@
-// estas funciones son de ejemplo
+import dataStudio from './data/ghibli/ghibli.js'
+//es resoponsable de toda la logica
 
-export const example = () => {
-  return 'example';
-};
+export function sendData(){
+  return dataStudio.films;
+}
+// let data = sendData();
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// const hayaoDirector = data.filter(film => film.director == 'Hayao Miyazaki' )
+// const peopleHayo = hayaoDirector.filter(people => people.name)
+
+// function people(films) {
+//   peopleDirectors = {};
+
+//   films.forEach(film => {
+//     let titleFilm = film.title.toLowerCase().replaceAll(' ', '_'); 
+//     peopleDirectors[director] = {};
+//     peopleDirectors[director][]
+
+//   })
+
+
+// }
+
+
+// let data = sendData();
+
+
+// function filterPeople(film) {
+//  let person = [];
+//  for (let i = 0; i < film.length; i++) {
+//    let people = film[i].people;
+//    for (let i in people) {
+//      if(people[i].specie === 'Cat') {
+//        person.push(people[i].name)
+//      }
+//    }
+//  }
+//  console.log(person);
+// }
+// filterPeople(data)
+
+
+//función para obtener el id y título de cada pelicula
+export function getIdTitles(films) {
+  let idTitles = [];
+
+  films.forEach(film => {
+    // console.log(film.id, film.title);
+    idTitles.push({
+      id: film.id,
+      title: film.title
+    });
+  });
+
+  idTitles.sort(function (a, b) {
+    if (a.title > b.title) {
+      return 1;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 0;
+
+  })
+
+ return (idTitles);
+}
+
+
+
+export function getIdDirectors(films) {
+
+  let directorsHash = {};
+
+    films.forEach(film => {
+      //creando la llave del objeto, en minusculas y sin espacios.
+      let keyDirector = film.director.toLowerCase().replaceAll(' ', '_'); 
+      //como es la primera pelicula que se consulta del ciclo, no existe la llave en directorHash
+      //film.director: nombre completo sin transformaciones.
+      if(directorsHash[keyDirector] === undefined) {
+        directorsHash[keyDirector] = {}; 
+        directorsHash[keyDirector]['name_director'] = film.director; 
+        directorsHash[keyDirector]['id_films'] =  []; 
+        directorsHash[keyDirector]['id_films'].push(film.id);
+        
+      }else{
+        //este paso sucede cuando ya se le ha creado una key al director y se necesita agregar un nuevo id de pelicula al mismo director en id_films
+        //esto sucede cuando el director tiene mas de una pelicula
+        directorsHash[keyDirector]['id_films'].push(film.id); 
+      }
+    });
+
+  return(directorsHash);
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,322 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// el archivo main es encargado de todo el html (dom)
 
-console.log(example, data);
+import { sendData,  getIdTitles, getIdDirectors} from './data.js';
+let allData = sendData();
+
+let filmSelect = document.getElementById('films');
+let container = document.getElementById('container');
+let directors = document.getElementById('directors');
+let infoFilms = document.getElementById('infoFilms');
+let orderFilm = document.getElementById('orderFilm');
+let orderFilm2 = document.getElementById('orderFilm2');
+let order_film = document.getElementById('order_film');
+let buttonInicio = document.getElementById('button-inicio');
+
+let allDirectors = getIdDirectors(allData);
+// let datosFilm = datosFilm(allData);
+// let button1 = document.getElementById('button-1');
+// let button2 = document.getElementById('button-2');
+
+ //creamos las opciones y se las agregamos al padre select con appendChild
+ //en cada recorrido por getIdTitles se crea una 'option'en select
+ //text, lo que el usuario ve y value, evalua el texto por id
+
+//Para el select de ordenado
+ 
+function selectOrder(){
+
+  let option1 = document.createElement('option');
+  let option2 = document.createElement('option');
+
+  option1.text = 'Descendente';
+  option1.value = 'Ascendente';
+  option2.text = 'Ascendente';
+  option2.value = 'Descendente';
+  order_film.appendChild(option1);
+  order_film.appendChild(option2);
+
+  let datosFilm = [];
+  
+
+  allData.forEach(film => {
+    datosFilm.push({
+      title: film.title,
+      id: film.id,
+      poster: film.poster
+    }) 
+  
+  
+  datosFilm.sort(function (a, b) {
+    if (a.title > b.title) {
+      return 1;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 0;
+  })
+
+  
+
+  order_film.addEventListener("change", () => {
+  
+    let html2 = '';
+    let html3 = '';
+    let select = (order_film.value);
+    container.innerHTML = ''
+
+    datosFilm.forEach(film => {
+
+      
+      if(select === 'Ascendente') {
+
+        html2 += `<div id="wrapper-grid" class="wrapper-grid">
+                    <div class="container-2">
+                    <img class="img-poster" src="${film.poster}">
+                      <div class="capa">
+                      <h2 class="film-title">${film.title}</h2>
+                      </div>
+                  </div>
+                </div>`
+      }
+      
+    })
+    
+    let reverse = datosFilm.reverse();
+    reverse.forEach(film => {
+
+       if (select === 'Descendente') {
+        
+          html3 += `<div id="wrapper-grid" class="wrapper-grid">
+                    <div class="container-2">
+                    <img class="img-poster" src="${film.poster}">
+                      <div class="capa">
+                      <h2 class="film-title">${film.title}</h2>
+                      </div>
+                  </div>
+                </div>`
+      }
+
+    })
+    orderFilm.innerHTML= html2;   
+    orderFilm2.innerHTML= html3;
+    
+    
+  });
+ })
+
+// console.log(datosFilm)
+
+    
+}
+
+
+selectOrder();
+  
+
+
+
+//////////////////////////////
+
+
+
+function fillSelect() {
+  let optionAll = document.createElement('option');
+  optionAll.text = 'Filmography';
+  optionAll.value = 'all';
+  filmSelect.appendChild(optionAll); 
+
+  getIdTitles(allData).forEach(idTitle => {
+    let option = document.createElement('option');
+
+    option.text = [idTitle.title]; 
+    option.value = idTitle.id; 
+    filmSelect.appendChild(option);
+  });
+  let html = "";
+ 
+  allData.forEach(film => {
+
+    let nuevoElemento = document.createElement("div");
+    nuevoElemento.setAttribute("id", film.id)
+    nuevoElemento.setAttribute("class", "wrapper-grid")
+
+   html = `<div class="container-2">
+              <img class="img-poster" src="${film.poster}">
+              <div class="capa">
+                <h2 class="film-title">${film.title}</h2>
+              </div>
+            </div>
+            `
+  nuevoElemento.innerHTML = html
+
+  container.appendChild(nuevoElemento)
+ 
+  
+  nuevoElemento.addEventListener("click", () => {
+
+    
+     allData.forEach(film => {
+     
+       if (nuevoElemento.id === film.id) {
+      
+         container.innerHTML = ''
+         infoFilms.innerHTML =
+          `<div class="divSection">
+            <section class="poster-section">
+              <h2 class="film-title">${film.title}</h2>
+              <img src="${film.poster}">
+            </section>
+              <h3 class="description">${film.description}</h3>
+
+              <div class="section-people">
+                    <div class="container-people">${film.people.map(person => {
+                    return `${person.name} <img src="${person.img}"></div>`;
+
+
+              })}</div>
+             
+           
+       `
+       
+       }
+
+     })
+      
+    });
+  })
+}
+
+filmSelect.addEventListener("change", function() {
+  let getId = (filmSelect.value)
+  let html = "";
+  
+  allData.forEach(film => {
+   
+    if(getId === film.id) {
+
+      html += `<div id="wrapper-grid" class="wrapper-grid">
+                <div class="container-2">
+                <img class="img-poster" src="${film.poster}">
+                  <div class="capa">
+                  <h2 class="film-title">${film.title}</h2>
+                  </div>
+              </div>
+            </div>`
+    } else if(getId === "all") {
+      html += `<div id="wrapper-grid" class="wrapper-grid">
+                <div class="container-2">
+                  <img class="img-poster" src="${film.poster}">
+                    <div class="capa">
+                    <h2 class="film-title">${film.title}</h2>
+                    </div>
+                </div>
+              </div>`
+    }
+  });
+  container.innerHTML = html;
+})
+
+//Agregar opciones al select de Directors, que seria 'directors'
+function selectDirectors() {
+  let optionAll = document.createElement('option');
+  optionAll.text = 'Directors';
+  optionAll.value = 'all';
+  directors.appendChild(optionAll); //creamos las opciones y se las agregamos al padre select
+
+
+  Object.keys(allDirectors).forEach(idDirector => {
+    
+    let option = document.createElement('option'); //por cada array recorrido se crea una opcion
+
+    option.text = allDirectors[idDirector].name_director; //text es lo que el usuario ve
+    option.value = idDirector; // value, por medio de que valor se evalua el texto. Por id
+  
+    directors.appendChild(option);//creamos las opciones y se las agregamos al padre select
+  });
+}
+
+directors.addEventListener("change", function() {
+  let idDirector = directors.value; //valor del select. directors es el select
+  let html = "";
+  let idDirectorFilms = allDirectors[idDirector].id_films; //todos los id de sus peliculas
+  
+  //que pelicula va impirmir por cada director
+  //este recorre toda la data
+  container.innerHTML = ''
+  allData.forEach(film => {
+
+    idDirectorFilms.forEach(idDirectorFilm => {
+          
+          if(idDirectorFilm === film.id) {
+
+            console.log('hjklñ')
+
+            let nuevoElemento = document.createElement("div");
+            nuevoElemento.setAttribute("id", film.id)
+            nuevoElemento.setAttribute("class", "wrapper-grid")
+
+            html = `<div class="container-2">
+            <img class="img-poster" src="${film.poster}">
+            <div class="capa">
+              <h2 class="film-title">${film.title}</h2>
+            </div>
+          </div>
+          `
+          nuevoElemento.innerHTML = html
+
+          container.appendChild(nuevoElemento)
+
+          nuevoElemento.addEventListener("click", () => {
+
+            
+             allData.forEach(film => {
+             
+               if (nuevoElemento.id === film.id) {
+              
+                 container.innerHTML = ''
+                 infoFilms.innerHTML =
+                  `<h2 class="film-title">${film.title}</h2>
+                   <img src="${film.poster}">
+                   <h3 class="description">${film.description}</h3>
+                   <ul>${film.people.map((person) => {
+                        return `<li>${person.name}</li>`;
+                   })}</ul>
+                   <div>${film.people.map((person) => {
+                    return `<img src="${person.img}">`;
+               })}</div>`
+               
+               }
+        
+             })
+              
+            });
+
+
+            // html += `<div id="wrapper-grid" class="wrapper-grid">
+            //             <div class="container-2">
+            //             <img class="img-poster" src="${film.poster}">
+            //               <div class="capa">
+            //               <h2 class="film-title">${film.title}</h2>
+            //               </div>
+            //           </div>
+            //         </div>`
+          }  
+    });
+  });
+  
+  
+  // container.innerHTML = html;
+
+});
+
+
+buttonInicio.addEventListener('click', () => {
+  console.log('hola');
+  container.innerHTML = ''
+  fillSelect();
+})
+
+
+fillSelect();
+selectDirectors();
